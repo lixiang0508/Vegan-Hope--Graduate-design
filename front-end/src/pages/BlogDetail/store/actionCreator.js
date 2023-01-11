8b695a32def31119cd710f6e29cb8a85b8c13ad0
lixiang0508 <79568031+lixiang0508@users.noreply.github.com>
2023-01-11 11:02:39 +0100

    Add files via upload

---
+++ front-end/src/pages/BlogDetail/store/actionCreator.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import {CHANGE_BLOGDTEAIL} from "./constant";
+
+const changeDetail = (result) =>(
+    {
+        type:CHANGE_BLOGDTEAIL,
+        title:result.title,
+        content:result.text,
+        img:result.img,
+        hits:result.hits,
+        likes:result.likes,
+        comments:result.comments
+
+    }
+)
+export const getBlogDetail =(id)=>{
+    return (dispatch)=>{
+        axios.get('http://localhost:8085/blog/get/'+id).then(res =>{
+            console.log('blog detail is',res.data.data);
+            //console.log('current img is',res.data.data.img);
+            const result = res.data.data;
+            dispatch(changeDetail(result))
+
+            }
+
+
+        )
+    }
+}
